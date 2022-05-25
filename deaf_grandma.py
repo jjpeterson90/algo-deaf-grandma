e37ea6000d0c3b3c6fa8391641fac39334d47740
Justin <jjpeterson90@hotmail.com>
2022-05-25 16:06:57 -0500

    complete

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,32 @@
-def deaf_grandma():
+from operator import truediv
 
+
+def deaf_grandma():
+    import random
+
+    def is_upper_case(input):
+        for i in range(len(input)):
+            if input[i] == input[i].lower():
+                return False
+        return True
+
+    goodbye_counter = 0
+    reply = input("HEY, KID!")
+
+    while goodbye_counter < 2:
+        if reply == "GOODBYE!":
+            if goodbye_counter == 1:
+                input("LATER, SKATER!")
+                goodbye_counter += 1
+            else:
+                reply = input("LEAVING SO SOON?")
+                goodbye_counter += 1
+        elif reply == "":
+            reply = input("WHAT?!")
+        elif is_upper_case(reply):
+            reply = input(f'NO, NOT SINCE {random.randrange(1935,1955)}')
+        else:
+            reply = input("SPEAK UP, KID!")
 
 
 deaf_grandma()
